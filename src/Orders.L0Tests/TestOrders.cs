215f3a16e10d281caf1e12f425534f805c288c04
Brian Elgaard Bennett <bb@saxobank.com>
2021-04-09 13:42:09 +0200

    Switched from MS Test to Xunit

--- src/Orders.L0Tests/TestOrders.cs
+++ src/Orders.L0Tests/TestOrders.cs
@@ -3,34 +3,34 @@ using System.Text;
 using System.Threading.Tasks;
 using LeanTest;
 using LeanTest.Core.ExecutionHandling;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LeanTest.MSTest;
+using LeanTest.Xunit;
+using Xunit;
 using Orders.L0Tests.Mocks;
 using Orders.Orders;
 using Orders.SharedDomain;
+using Xunit.Abstractions;
 using TestScenarioIdAttribute = LeanTest.Attribute.TestScenarioIdAttribute;
 
 namespace Orders.L0Tests
 {
-    [TestClass]
     public class TestOrders
     {
         private ContextBuilder _contextBuilder;
         private HttpClient _target;
         private IQtePlacedOrderReader _qtePlacedOrderReader;
-        public TestContext TestContext { get; set; }
 
-        [TestInitialize]
-        public void TestInitialize()
+        public TestOrders(ITestOutputHelper output)
         {
+            var testContext = new TestContext(output);
             _contextBuilder = ContextBuilderFactory.CreateContextBuilder()
-                .RegisterAttributes(TestContext);
+                .RegisterAttributes(testContext)
+                .Build();
 
             _qtePlacedOrderReader = _contextBuilder.GetInstance<IQtePlacedOrderReader>();
             _target = _contextBuilder.GetHttpClient();
         }
 
-        [TestMethod, TestScenarioId("Core")]
+        [Fact, TestScenarioId("Core")]
         public async Task PostOrderMustBuyWhenAssetIsTradable()
         {
             // Declare IC:
@@ -41,11 +41,11 @@ namespace Orders.L0Tests
             HttpResponseMessage actual = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.ValidBuy, Encoding.UTF8, "application/json"));
 
-            Assert.IsTrue(actual.IsSuccessStatusCode);
-            Assert.AreEqual((TestData.ValidInstrumentId, BuySell.Buy), _qtePlacedOrderReader.Query()); // Observe the difference between IC and the resulting state. 
+            Assert.True(actual.IsSuccessStatusCode);
+            Assert.Equal((TestData.ValidInstrumentId, BuySell.Buy), _qtePlacedOrderReader.Query()); // Observe the difference between IC and the resulting state. 
         }
 
-        [TestMethod, TestScenarioId("Core")]
+        [Fact, TestScenarioId("Core")]
         public async Task PostOrderMustSellWhenAssetIsTradable()
         {
             // Declare IC:
@@ -56,11 +56,11 @@ namespace Orders.L0Tests
             HttpResponseMessage actual = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.ValidSell, Encoding.UTF8, "application/json"));
 
-            Assert.IsTrue(actual.IsSuccessStatusCode);
-            Assert.AreEqual((TestData.ValidInstrumentId, BuySell.Sell), _qtePlacedOrderReader.Query()); // Observe the difference between IC and the resulting state. 
+            Assert.True(actual.IsSuccessStatusCode);
+            Assert.Equal((TestData.ValidInstrumentId, BuySell.Sell), _qtePlacedOrderReader.Query()); // Observe the difference between IC and the resulting state. 
         }
 
-        [TestMethod, TestScenarioId("Core")]
+        [Fact, TestScenarioId("Core")]
         public async Task PostOrderMustNotBuyWhenAssetIsNotTradable()
         {
             // Declare IC:
@@ -71,12 +71,12 @@ namespace Orders.L0Tests
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.ValidBuy, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("not allowed to trade"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("not allowed to trade"));
         }
 
-        [TestMethod, TestScenarioId("Core")]
+        [Fact, TestScenarioId("Core")]
         public async Task PostOrderMustNotSellWhenAssetIsNotTradable()
         {
             // Declare IC:
@@ -87,125 +87,125 @@ namespace Orders.L0Tests
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.ValidSell, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("not allowed to trade"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("not allowed to trade"));
         }
 
-        [TestMethod, TestScenarioId("Input")]
+        [Fact, TestScenarioId("Input")]
         public async Task PostOrderMustReportErrorWhenInvalidId()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidId, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Invalid Id"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Invalid Id"));
         }
 
-        [TestMethod, TestScenarioId("Input")]
+        [Fact, TestScenarioId("Input")]
         public async Task PostOrderMustReportErrorWhenInvalidPrice()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidPrice, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Invalid price"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Invalid price"));
         }
 
-        [TestMethod, TestScenarioId("Input")]
+        [Fact, TestScenarioId("Input")]
         public async Task PostOrderMustReportErrorWhenInvalidAmount()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidAmount, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Invalid amount"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Invalid amount"));
         }
 
-        [TestMethod, TestScenarioId("Input")]
+        [Fact, TestScenarioId("Input")]
         public async Task PostOrderMustReportErrorWhenInvalidDuration()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidDuration, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Invalid duration"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Invalid duration"));
         }
 
-        [TestMethod, TestScenarioId("AdvancedInput")]
+        [Fact, TestScenarioId("AdvancedInput")]
         public async Task PostOrderMustReportErrorWhenInvalidPositionId()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidPositionId, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
         }
 
-        [TestMethod, TestScenarioId("AdvancedInput")]
+        [Fact, TestScenarioId("AdvancedInput")]
         public async Task PostOrderMustReportErrorWhenPositionIdNotThere()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.PositionIdNotThere, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
         }
 
-        [TestMethod, TestScenarioId("AdvancedInput")]
+        [Fact, TestScenarioId("AdvancedInput")]
         public async Task PostOrderMustReportErrorWhenDistanceNotThere()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.DistanceNotThere, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Distance"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Distance"));
         }
 
-        [TestMethod, TestScenarioId("AdvancedInput")]
+        [Fact, TestScenarioId("AdvancedInput")]
         public async Task PostOrderMustReportErrorWhenStepNotThere()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.StepNotThere, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Step"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Step"));
         }
 
-        [TestMethod, TestScenarioId("AdvancedInput")]
+        [Fact, TestScenarioId("AdvancedInput")]
         public async Task PostOrderMustReportErrorWhenInvalidDistance()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidDistance, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Distance"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Distance"));
         }
 
-        [TestMethod, TestScenarioId("AdvancedInput")]
+        [Fact, TestScenarioId("AdvancedInput")]
         public async Task PostOrderMustReportErrorWhenInvalidStep()
         {
             // No IC to declare!
             HttpResponseMessage response = await 
                 _target.PostAsync("/orders/place-order", new StringContent(TestData.RelatedOrders.InvalidStep, Encoding.UTF8, "application/json"));
 
-            Assert.IsFalse(response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             var actual = await response.Content.ReadAsAsync<GenericOrderResponse>();
-            Assert.IsTrue(actual.ErrorInfo.ErrorCode.Contains("Step"));
+            Assert.True(actual.ErrorInfo.ErrorCode.Contains("Step"));
         }
     }
 }
