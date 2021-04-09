466de7275ddeacc77417ac432545dc753bb7ae26
Brian Elgaard Bennett <bb@saxobank.com>
2021-04-09 13:28:59 +0200

    Empty L2

---
+++ Orders.L2Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orders.L2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
