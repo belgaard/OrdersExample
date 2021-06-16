99a232c5c54aff888f279739f63a667686c0cc77
Brian Elgaard <belgaard@users.noreply.github.com>
2021-06-16 13:38:47 +0200

    Added Docker F5 debug-ability (#6)
    Co-authored-by: Brian Elgaard Bennett <bb@saxobank.com>

--- src/orders/Qte/Qte.cs
+++ src/orders/Qte/Qte.cs
@@ -11,38 +11,6 @@ using Orders.SharedDomain;
 
 namespace Orders.Qte
 {
-    public static class OrderMapping
-    {
-        /// <summary>Maps PlaceableOrderType to the external <see cref="OpenOrderType"/>.</summary>
-        public static OpenOrderType MapOrderType(PlaceableOrderType? orderType)
-        {
-            switch (orderType.GetValueOrDefault())
-            {
-                case PlaceableOrderType.Market:
-                    return OpenOrderType.Market;
-                case PlaceableOrderType.Limit:
-                    return OpenOrderType.Limit;
-                case PlaceableOrderType.TrailingStop:
-                    return OpenOrderType.Stop;
-                default:
-                    throw new ArgumentOutOfRangeException("Cannot map orderType '" + orderType.GetValueOrDefault() + "' to FrontOffice.Trading.Public.OpenOrderType.");
-            }
-        }
-        /// <summary>Maps our OrderDuration to external <see cref="OpenOrderDuration"/>.</summary>
-        public static OpenOrderDuration? MapOrderDuration(OrderDuration orderDuration)
-        {
-            if (orderDuration == null) throw new ArgumentNullException(nameof(orderDuration));
-
-            switch (orderDuration.DurationType)
-            {
-                case OrderDurationType.GoodTillDate:
-                    return OpenOrderDuration.GoodTillDate;
-                default:
-                    throw new ArgumentOutOfRangeException("Cannot map orderDuration.DurationType '" + orderDuration.DurationType + "' to FrontOffice.Trading.Public.OpenOrderDuration");
-            }
-        }
-    }
-
     public class Qte
     {
         private readonly ISessionTradesFacade _session;
